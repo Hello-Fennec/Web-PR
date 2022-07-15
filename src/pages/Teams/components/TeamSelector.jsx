96343be1b8b939d4b0e17a218c3d2b79ece1dc95
Jerome <naronkrach@gmail.com>
2022-07-15 11:59:39 +0700

    Adjust selector to hidden on mobile

--- src/pages/Teams/components/TeamSelector.jsx
+++ src/pages/Teams/components/TeamSelector.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { useState } from "react";
+import BREAKPOINTS from "../../../constants/BREAKPOINTS";
 import TEAMS from "../../../constants/TEAMS";
 
 export default function TeamSelector() {
-    const [selectedTeam, setselectedTeam] = useState(0);
-  const Selector = ({index}) => <a
+  const [selectedTeam, setselectedTeam] = useState(0);
+  const Selector = ({ index }) => (
+    <a
       onClick={() => {
-        console.log({index});
+        console.log({ index });
         setselectedTeam(index);
       }}
-      className={(selectedTeam == index? "bg-red-500":"bg-gray-500") + " w-20 h-20 bg-gray-500 cursor-pointer rounded-2xl"}
-    ></a>
-  
-  return <div className="selectorContainer flex flex-row justify-center items-end space-x-5  lg:flex-col lg:space-x-0 lg:space-y-5 lg:mr-5">
-      {TEAMS.map((team, index) => {
-        return <Selector key={index} index={index}/>
+      className={
+        (selectedTeam == index ? "bg-red-500" : "bg-gray-500") +
+        " w-20 h-20 bg-gray-500 cursor-pointer rounded-2xl"
       }
-        )}
-  </div>;
+    ></a>
+  );
+
+  return (
+    <div className="selectorContainer sm:hidden flex flex-row justify-center items-end space-x-5  lg:flex-col lg:space-x-0 lg:space-y-5 lg:mr-5">
+      {TEAMS.map((team, index) => {
+        return <Selector key={index} index={index} />;
+      })}
+    </div>
+  );
 }
