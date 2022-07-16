80333027cc4cf4957ac39181874cf5506b384061
Jerome <naronkrach@gmail.com>
2022-07-16 12:52:44 +0700

    Added mobile faqs toggle btn

--- src/pages/FAQs/FAQs.jsx
+++ src/pages/FAQs/FAQs.jsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
+import BREAKPOINTS from "../../Data/BREAKPOINTS";
 import FAQS from "../../Data/FAQS";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 import FaqsToggle from "./components/FaqsToggle";
 
 function Faqs() {
   const [current, setCurrent] = useState(0);
+  const { height, width } = useWindowDimensions();
+
+  const MobileFaqs = () => {
+    return (
+      <div className="flex flex-col items-center space-y-2">
+        {FAQS.map((faq, index) => {
+          return (
+            <FaqsToggle
+              key={index}
+              index={index}
+              current={current}
+              setCurrent={setCurrent}
+              question={faq.question}
+              answer={faq.answer}
+            />
+          );
+        })}
+      </div>
+    );
+  };
 
   const DesktopFaqs = () => {
     return (
@@ -23,14 +45,14 @@ function Faqs() {
           })}
         </div>
         <div className="w-1/2  bg-gray-300 ml-2 p-2 flex flex-col justify-center items-center rounded-lg ">
-          <div className="text-xl">{FAQS[current].question}</div>
+          <div className="text-xl">{current>-1&&FAQS[current].question}</div>
           <br />
-          <div className="text-2xl font-semibold ">{FAQS[current].answer}</div>
+          <div className="text-2xl font-semibold ">{current>-1?FAQS[current].answer:"Click to see"}</div>
         </div>
       </div>
     );
   };
-  return <DesktopFaqs />;
+  return width < BREAKPOINTS.mobile ? <MobileFaqs /> : <DesktopFaqs />;
 }
 
 export default Faqs;
--- src/pages/FAQs/components/FaqsToggle.jsx
+++ src/pages/FAQs/components/FaqsToggle.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import BREAKPOINTS from "../../../Data/BREAKPOINTS";
 
-export default function FaqsToggle({ current, setCurrent, index, title }) {
+export default function FaqsToggle({
+  current,
+  setCurrent,
+  index,
+  title,
+  question,
+  answer,
+}) {
   return (
-    <button
-      onClick={() => setCurrent(index)}
-      className={
-        (current === index ? "bg-red-500" : "bg-gray-500") +
-        " p-5 w-72 h-full rounded-lg"
-      }
-    >
-      {title}
-    </button>
+    <div className=" flex sm:w-11/12 flex-col items-center">
+      <button
+        onClick={() => index === current ? setCurrent(-1): setCurrent(index)}
+        className={
+          (current === index ? "bg-red-500" : "bg-gray-500") +
+          " p-5 w-72 sm:w-full h-full rounded-lg sm:text-sm"
+        }
+      >
+        {title}
+        {question}
+      </button>
+      <div
+        className={
+          (index === current && "sm:flex") +
+          " w-full h-32 hidden bg-slate-300 -z-10 p-2 flex-col justify-center items-center"
+        }
+      >
+        {answer}
+      </div>
+    </div>
   );
 }
