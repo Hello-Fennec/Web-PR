d1da4fc140b80035d6abf08a84c88cc6e83e0309
Jerome <naronkrach@gmail.com>
2022-07-16 23:29:20 +0700

    Fixed faqs mobile toggle z index

--- src/pages/FAQs/components/FaqsToggle.jsx
+++ src/pages/FAQs/components/FaqsToggle.jsx
@@ -14,7 +14,7 @@ export default function FaqsToggle({
         onClick={() => (index === current ? setCurrent(-1) : setCurrent(index))}
         className={
           (current === index ? "bg-red-500 sm:mb-0" : "bg-gray-500") +
-          " p-5 w-72 sm:w-full h-full rounded-lg sm:text-sm mb-2"
+          " p-5 w-72 sm:w-full h-full rounded-lg sm:text-sm mb-2 z-10"
         }
       >
         {title}
@@ -24,7 +24,7 @@ export default function FaqsToggle({
         id="ansContainer"
         className={
           (index === current ? "sm:flex " : "") +
-          " h-32 w-full hidden opacity-1 bg-slate-300 -z-10 p-2 flex-col justify-center items-center mb-2 duration-300"
+          " h-32 w-full hidden opacity-1 bg-slate-300 p-2 flex-col justify-center items-center mb-2 duration-300"
         }
       >
         {answer}
