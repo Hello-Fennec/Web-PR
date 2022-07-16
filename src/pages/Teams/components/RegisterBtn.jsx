937c236a33ba532b8b7c4545aa92d04800fb19d0
Jerome <naronkrach@gmail.com>
2022-07-16 13:00:37 +0700

    Fixed register btn position

--- src/pages/Teams/components/RegisterBtn.jsx
+++ src/pages/Teams/components/RegisterBtn.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const RegisterBtn = () => {
   return (
-    <div>
-      <a href="#" className="absolute p-4 w-1/3 sm:w-2/5 mb-10 md:mb-5 sm:mb-3 md:mr-3 md:right-0 md:-translate-x-0 rounded-md bg-gray-500 bottom-0 -translate-x-1/2 hover:-translate-y-1 duration-500">
-        Register Now!
-      </a>
-    </div>
+    <a
+      href="#"
+      className="absolute p-4 w-1/3 mb-10 sm:w-2/5 sm:mb-3 
+        md:mb-5 md:mr-3 md:right-0 md:-translate-x-0
+        rounded-md bg-gray-500 bottom-0 
+         hover:-translate-y-1 duration-500"
+    >
+      Register Now!
+    </a>
   );
 };
 
