af2b4c9d9eb88ffc1630185aa3656e13bab5d8b1
Jerome <naronkrach@gmail.com>
2022-07-17 15:52:31 +0700

    Added faqs toggle hover effect

--- src/pages/FAQs/components/FaqsToggle.jsx
+++ src/pages/FAQs/components/FaqsToggle.jsx
@@ -9,12 +9,12 @@ export default function FaqsToggle({
   answer,
 }) {
   return (
-    <div className=" flex sm:w-11/12 flex-col items-center">
+    <div className=" flex sm:w-11/12 flex-col items-center ">
       <button
         onClick={() => (index === current ? setCurrent(-1) : setCurrent(index))}
         className={
           (current === index ? "bg-red-500 sm:mb-0" : "bg-gray-500") +
-          " p-5 w-72 sm:w-full h-full rounded-lg sm:text-sm mb-2 z-10"
+          " p-5 w-72 sm:w-full h-full rounded-lg sm:text-sm mb-2 z-10 hover:-translate-y-1 duration-500"
         }
       >
         {title}
