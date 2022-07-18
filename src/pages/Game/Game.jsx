ef6e54fe83652a99362186e8370a2d26edb867d5
Jerome <naronkrach@gmail.com>
2022-07-18 17:06:56 +0700

    Extracted TapToPlay component

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,34 +1,12 @@
-import { motion } from "framer-motion";
-import PageAnimation from "../../components/PageAnimation";
+import TapToPlay from "./components/TapToPlay";
+
 
 function Game() {
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    ) ||
-    (/Macintosh/i.test(navigator.userAgent) && navigator.maxTouchPoints > 0);
 
-  const Playable = () => (
-    <div className="flex flex-col justify-center items-center">
-      <motion.a
-        whileHover={{
-          scale: [1, 1.3, 1.1],
-          transition: { duration: 0.5, ease: "easeInOut" },
-          rotate: [0, -20, 15, 0],
-        }}
-        href="#"
-        className="w-[36rem] h-80 bg-gray-500"
-      ></motion.a>
-    </div>
-  );
-
-  const Unplayable = () => (
-    <div className="flex flex-col justify-center items-center">
-      <div className="w-[90%] h-96  bg-gray-500"></div>
-    </div>
-  );
   return (
-    <PageAnimation>{isMobile ? <Unplayable /> : <Playable />}</PageAnimation>
+    <>
+    <TapToPlay/>
+    </>
   );
 }
 
---
+++ src/pages/Game/components/TapToPlay.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion } from "framer-motion";
+import PageAnimation from "../../../components/PageAnimation";
+
+export default function TapToPlay() {
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    ) ||
+    (/Macintosh/i.test(navigator.userAgent) && navigator.maxTouchPoints > 0);
+
+  const Playable = () => (
+    <div className="flex flex-col justify-center items-center">
+      <motion.a
+        whileHover={{
+          scale: [1, 1.3, 1.1],
+          transition: { duration: 0.5, ease: "easeInOut" },
+          rotate: [0, -20, 15, 0],
+        }}
+        href="#"
+        className="w-[36rem] h-80 bg-gray-500"
+      ></motion.a>
+    </div>
+  );
+
+  const Unplayable = () => (
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-[90%] h-96  bg-gray-500"></div>
+    </div>
+  );
+  return (
+    <PageAnimation>{isMobile ? <Unplayable /> : <Playable />}</PageAnimation>
+  );
+}
