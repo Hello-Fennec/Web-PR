0b80ff4f6fdeca63c409cad83b36843230c360e3
Jerome <naronkrach@gmail.com>
2022-07-21 19:10:14 +0700

    Editted game page

--- src/pages/Game/components/TapToPlay.jsx
+++ src/pages/Game/components/TapToPlay.jsx
@@ -10,28 +10,34 @@ export default function TapToPlay() {
     (/Macintosh/i.test(navigator.userAgent) && navigator.maxTouchPoints > 0);
 
   const Playable = () => (
-    <motion.a
-      whileHover={{
-        scale: [1, 1.05],
-        rotate: [0, 1, -1],
-        transition: {
-          duration: 0.3,
-          ease: "easeInOut",
-          repeat: Infinity,
-          repeatType: "reverse",
-        },
-      }}
-      href="#"
-      className="w-[36rem] h-80 bg-gray-500"
-    ></motion.a>
+    <div className="flex flex-col justify-center items-center">
+      <motion.a
+        whileHover={{
+          scale: [1, 1.05],
+          rotate: [0, 1, -1],
+          transition: {
+            duration: 0.3,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        }}
+        href="#"
+        className="w-[36rem] h-80 bg-gray-500"
+      ></motion.a>
+    </div>
   );
 
-  const Unplayable = () => <div className="w-[90%] h-96  bg-gray-500"></div>;
-  return (
+  const Unplayable = () => (
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-[90%] h-96  bg-gray-500"></div>
+    </div>
+  );
+  return isMobile ? (
+    <Unplayable />
+  ) : (
     <PageAnimation>
-      <div className="flex flex-col justify-center items-center">
-        {isMobile ? <Unplayable /> : <Playable />}
-      </div>
+      <Playable />
     </PageAnimation>
   );
 }
