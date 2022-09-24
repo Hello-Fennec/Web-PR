5cbc3a4a9134e2c621a9c23dacab29caa82c8909
Naronkrach Tanajarusawas <naronkrach@gmail.com>
2022-09-24 16:07:59 +0700

    Added target blank to tap-to-play and edit theme color in meta tag

--- src/pages/Game/components/TapToPlay.jsx
+++ src/pages/Game/components/TapToPlay.jsx
@@ -12,30 +12,30 @@ export default function TapToPlay() {
     (/Macintosh/i.test(navigator.userAgent) && navigator.maxTouchPoints > 0);
 
   const Playable = () => (
-      <div className="flex flex-col justify-center items-center">
-        <motion.a
-          whileHover={{
-            scale: [1, 1.05],
-            rotate: [0, 1, -1],
-            transition: {
-              duration: 0.3,
-              ease: "easeInOut",
-              repeat: Infinity,
-              repeatType: "reverse",
-            },
-          }}
-          href="
+    <div className="flex flex-col justify-center items-center">
+      <motion.a
+        whileHover={{
+          scale: [1, 1.05],
+          rotate: [0, 1, -1],
+          transition: {
+            duration: 0.3,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        }}
+        href="
         http://game-helloworldfennec.sit.kmutt.ac.th/"
-          className="w-[50rem] h-[30rem]  2xl:scale-125"
-        >
-    <ImgContainer src={TapToPlayBtn} className="w-full h-full"/>
-
-        </motion.a>
-      </div>
+        target="_blank"
+        className="w-[50rem] h-[30rem]  2xl:scale-125"
+      >
+        <ImgContainer src={TapToPlayBtn} className="w-full h-full" />
+      </motion.a>
+    </div>
   );
   const Unplayable = () => (
     <div className="w-screen flex flex-col justify-center items-center">
-      <div className="w-[90%] max-w-[50rem] h-[32rem]  bg-gray-500  "></div>
+      <div className="w-[90%] max-w-[50rem] h-[32rem] bg-gray-500  "></div>
     </div>
   );
   return isMobile ? (
