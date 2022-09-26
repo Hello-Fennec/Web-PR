f22ad718874dc53e1206e4a2b9ccf2a5d0bc2865
Naronkrach Tanajarusawas <naronkrach@gmail.com>
2022-09-26 10:39:10 +0700

    Editted bottom fixed layout

--- src/components/RegisterBtn.jsx
+++ src/components/RegisterBtn.jsx
@@ -3,12 +3,12 @@ import { TbEdit } from "react-icons/tb";
 
 const RegisterBtn = () => {
   return (
-    <div className="SocialMedia fixed bottom-0 right-0 flex mb-10 mr-10 md:mb-5 md:mr-5 sm:mb-3 sm:mr-3 text-white space-x-4 sm:space-x-2 z-50 2xl:scale-125 origin-bottom-right">
+    <div className=" fixed bottom-0 right-0 flex mb-10 mr-10 md:mb-5 md:mr-5 sm:mb-3 sm:mr-3 text-white space-x-4 sm:space-x-2 z-50 2xl:scale-125 origin-bottom-right">
       <a
         href="#"
         target="_blank"
         className="bg-[#a15d00] text-center text-xl rounded-full flex justify-center items-center 
-        hover:-translate-y-1 hover:bg-red-500 duration-200 transition-all 
+        hover:-translate-y-3 hover:scale-110 hover:bg-[#DF963A] hover:shadow-md duration-200 transition-all 
         h-20 w-44  md:h-16 md:w-16 sm:h-14 sm:w-14 "
       >
         <TbEdit size={30} />
