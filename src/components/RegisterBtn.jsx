ab23ca3c01bcf794594bb6dd61bde1b036d9ad10
Naronkrach Tanajarusawas <naronkrach@gmail.com>
2022-09-29 11:34:50 +0700

    Added link to register btn

--- src/components/RegisterBtn.jsx
+++ src/components/RegisterBtn.jsx
@@ -5,7 +5,7 @@ const RegisterBtn = () => {
   return (
     <div className=" fixed bottom-0 right-0 flex mb-10 mr-10 md:mb-5 md:mr-5 sm:mb-3 sm:mr-3 text-white space-x-4 sm:space-x-2 z-50 2xl:scale-125 origin-bottom-right">
       <a
-        href="#"
+        href="https://forms.gle/16JsWhnKX6ecjCpTA"
         target="_blank"
         className="bg-[#a15d00] text-center text-xl rounded-full flex justify-center items-center 
         hover:-translate-y-3 hover:scale-110 hover:bg-[#ca8018] hover:shadow-md duration-200 transition-all 
