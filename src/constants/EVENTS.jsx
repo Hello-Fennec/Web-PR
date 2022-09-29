9c5ed5b0f716aa38c16b941d6f678c1bdd0055e9
Naronkrach Tanajarusawas <naronkrach@gmail.com>
2022-09-29 14:32:38 +0700

    Restructured events section

--- src/constants/EVENTS.jsx
+++ src/constants/EVENTS.jsx
@@ -1,16 +1,49 @@
-export const today = new Date()
-export const registerDay = new Date("2022-10-7");
-export const announceDay = new Date("2022-10-25");
-// export const firstMeetDay = new Date("2022-10-29");
-export const fennecDay = new Date("2022-10-29");
-export const graduationDay = new Date("2022-11-25");
-export function isPassed(date1, date2) {
+const today = new Date("2022-11-25");
+const registerDay = new Date("2022-10-7");
+const announceDay = new Date("2022-10-25");
+const fennecDay = new Date("2022-10-29");
+const graduationDay = new Date("2022-11-25");
+function isPassed(date1, date2) {
   return date1 >= date2;
 }
-export const allEvents = [
-  registerDay.toDateString(),
-  announceDay.toDateString(),
-  // firstMeetDay.toDateString(),
-  fennecDay.toDateString(),
-  graduationDay.toDateString(),
+
+export const ListOfEvents = [
+  {
+    title: "เปิดรับสมัคร",
+    date: registerDay,
+    thaiMonth: "ต.ค.",
+    description: "เฟ้นหาเฟนเนคตัวน้อย",
+    descriptionLine2: "เข้าสู่โรงเรียน HelloWorld !",
+    isPassed: isPassed(today, registerDay),
+  },
+  {
+    title: "ประกาศผล",
+    date: announceDay,
+    thaiMonth: "ต.ค.",
+    description: "ประกาศรายชื่อเหล่าเฟนเนค",
+    descriptionLine2: "ที่ได้รับคัดเลือก !",
+    isPassed: isPassed(today, announceDay),
+  },
+  {
+    title: "Fennec day",
+    date: fennecDay,
+    thaiMonth: "ต.ค.",
+    description: "โรงเรียนเปิดแล้ว",
+    descriptionLine2: " ได้เวลาฝึกฝนทักษะ !",
+    isPassed: isPassed(today, fennecDay),
+  },
+  {
+    title: "Graduation",
+    date: graduationDay,
+    thaiMonth: "พ.ย.",
+    description: "จบการศึกษาแล้ว ! ไชโย",
+    descriptionLine2: "",
+    isPassed: isPassed(today, graduationDay),
+  },
 ];
+
+const allEvents = Array.from(
+  ListOfEvents.map((event) => event.date.toDateString())
+);
+
+export const onEvent = allEvents.indexOf(today.toDateString()) + 1;
--- src/pages/Timeline/Events.jsx
+++ src/pages/Timeline/Events.jsx
@@ -1,56 +1,46 @@
 import React from "react";
 import PageAnimation from "../../components/PageAnimation";
 import Event from "./components/Event";
-import {
-  today,
-  registerDay,
-  announceDay,
-  fennecDay,
-  isPassed,
-  allEvents,
-  graduationDay,
-} from "../../constants/EVENTS";
+import { ListOfEvents, onEvent } from "../../constants/EVENTS";
 const Events = () => {
+
   return (
     <PageAnimation>
       <div className="w-full  p-5 flex space-x-20 lg:space-x-0 justify-center 2xl:scale-110  ">
         <div className="lg:hidden w-[50vw] bg-[#a15d00] rounded-2xl" />
         <div className=" flex flex-col space-y-7 lg:scale-125 md:scale-100 origin-center">
           <div
-            className="w-[1.5px] h-[33rem] absolute
+            className="w-1 h-[33rem] absolute
             -z-0 ml-12 mt-1 bg-gray-500"
-          ></div>
-          <Event
-            date={7}
-            month={"ต.ค."}
-            title={"เปิดรับสมัคร"}
-            description={"เฟ้นหาเฟนเนคตัวน้อย"}
-            descriptionLine2={"เข้าสู่โรงเรียน HelloWorld !"}
-            isPassed={isPassed(today, registerDay)}
-          />
-          <Event
-            date={25}
-            month={"ต.ค."}
-            title={"ประกาศผล"}
-            description={"ประกาศรายชื่อเหล่าเฟนเนค"}
-            descriptionLine2={"ที่ได้รับคัดเลือก !"}
-            isPassed={isPassed(today, announceDay)}
-          />
-          <Event
-            date={29}
-            month={"ต.ค."}
-            title={"Fennec day"}
-            description={"โรงเรียนเปิดแล้ว"}
-            descriptionLine2={" ได้เวลาฝึกฝนทักษะ !"}
-            isPassed={isPassed(today, fennecDay)}
-          />
-          <Event
-            date={25}
-            month={"พ.ย."}
-            title={"Graduation"}
-            description={"จบการศึกษาแล้ว ! ไชโย"}
-            isPassed={isPassed(today, graduationDay)}
-          />
+          >
+            <div
+              className={
+                (onEvent === 1
+                  ? "h-1/5"
+                  : onEvent === 2
+                  ? "h-2/5"
+                  : onEvent === 3
+                  ? "h-3/5"
+                  : onEvent === 4
+                  ? "h-full"
+                  : "h-0") + ` bg-[#a15d00] w-full bg-blend-overlay blend `
+              }
+            />
+          </div>
+
+          {ListOfEvents.map((event, index) => {
+            return (
+              <Event
+                key={index}
+                title={event.title}
+                date={event.date.getDate()}
+                month={event.thaiMonth}
+                description={event.description}
+                descriptionLine2={event.descriptionLine2}
+                isPassed={event.isPassed}
+              />
+            );
+          })}
         </div>
       </div>
     </PageAnimation>
